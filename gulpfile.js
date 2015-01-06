79d9124ccd7e95d21e99680c61a6b7983f238b5c
phi <phi1618jp@gmail.com>
2015-01-06 15:10:40 +0900

    fix gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+
+gulp.task("default", function() {
+  gulp.src(["co2.js"])
+    .pipe(uglify())
+    .pipe(rename({extname:'.min.js'}))
+    .pipe(gulp.dest("./"))
+});
